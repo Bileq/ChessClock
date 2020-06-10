6aacdd4055801fa40ed1862ee55be3c1de9b6316
Dominik Bil <dominobil112233@gmail.com>
2020-06-11 00:10:28 +0200

    Add settings icon

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import settings from './settings3.svg';
 
 function Header() {
     return (
         <header style={headerStyle}>
+             <button className='settingsBtn' style={{
+                  position: 'fixed',
+                  top: '90px',
+                  left: '15%',
+                  borderRadius: '25px',                  
+             }}>
+             <img src={settings} alt="settings icon" style={imgStyle} /></button>
             Chess Clock
         </header>
     );
@@ -13,9 +21,17 @@ const headerStyle = {
     padding: '60px',
     textAlign: 'center',
     fontSize: '104px',
-    backgroundColor: '#222',
     border: '1px solid red',
     opacity: '0.8',
-};
+}
+
+const imgStyle = {
+    backgroundColor: '#ff0',
+    height: '65px',
+    width: '65px',
+    borderRadius: '30px',
+}
+
+
 
 export default Header;
